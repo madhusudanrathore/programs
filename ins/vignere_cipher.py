f9da678d082e5c0a395bf45263495aa93b092b06
Madhusudan Rathore <rmadhusudan359@gmail.com>
2018-07-31 01:46:49 +0530

    Update vignere_cipher.py

--- ins/vignere_cipher.py
+++ ins/vignere_cipher.py
@@ -1,46 +1,48 @@
-key="M@d#U"
-key_len=len(key)
-
-input_str = "hellomadhusudan"
-inp_len=len(input_str)
-
-def padding(): # padding for remaining values
-	temp_str=""
-	temp_str+=input_str
-	if inp_len%key_len != 0:
-		temp=key_len-inp_len%key_len
+def padding(plain_text, key): # padding for remaining values
+	temp_str = ""
+	temp_str += plain_text
+	plain_text_len = len(plain_text)
+	key_len = len(key)
+	
+	if plain_text_len%key_len != 0:
+		temp = key_len-plain_text_len%key_len
 		for i in range(temp):
-			temp_str+="~"
+			temp_str += "~"
+	
 	return temp_str
 
-modified_input_str=padding()
-modified_len=len(modified_input_str)
+def encryption(final_plain_text, key):
+	enc_str = ""
+	final_plain_text_len = len(final_plain_text)
+	key_len = len(key)
 
-def encryption():
-	enc_str=""
-	x=0
-	while x < modified_len:
+	x = 0
+	while x < final_plain_text_len:
 		for y in range(key_len):
-			shift=ord(key[y])
-			char=modified_input_str[x+y]
-			if char==" ":
+			shift = ord(key[y])
+			char = final_plain_text[x+y]
+			if char == " ":
 				enc_str += char
 			elif char.islower():
 				enc_str += chr((ord(char)+shift-97)%26+97)
 			elif char.isupper():
 				dec_str += chr((ord(char)+shift-65)%26+65)
-			elif char=="~":
+			elif char == "~":
 				enc_str += '~'
 		x += key_len
+	
 	return enc_str
 
-def decryption(enc_str):
+def decryption(final_plain_text, key):
 	dec_str=""
+	final_plain_text_len = len(final_plain_text)
+	key_len = len(key)
+
 	x=0
-	while x < modified_len:
+	while x < final_plain_text_len:
 		for y in range(key_len):
 			shift=26-ord(key[y])
-			char=enc_str[x+y]
+			char=final_plain_text[x+y]
 			if char==" ":
 				dec_str += char
 			elif char.islower():
@@ -50,12 +52,24 @@ def decryption(enc_str):
 			elif char=="~":
 				dec_str += '~'
 		x += key_len
+	
 	return dec_str
 
-encrypted_str = encryption()
-decrypted_str = decryption(encrypted_str)
+def main():
+	key = "M@d#U"
+	plain_text = "hellomadhusudan"
+	cipher_text = None
+	decipher_text = None
 
-print ( "input:\t" + input_str )
-print ( "modified input:\t" + modified_input_str )
-print ( "encryption:\t" + encrypted_str )
-print ( "decryption:\t" + decrypted_str )
+	final_plain_text=padding(plain_text, key)
+	cipher_text = encryption(final_plain_text, key)
+	decipher_text = decryption(cipher_text, key)
+
+	print("PLAIN TEXT:\t" + plain_text)
+	print ("FINAL PLAIN TEXT:\t{}".format(final_plain_text))
+	print("CIPHER TEXT:\t" + cipher_text)
+	print("DECIPHER TEXT:\t" + decipher_text)
+
+
+if __name__ == '__main__':
+	main()
