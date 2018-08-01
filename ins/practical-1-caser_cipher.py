ae43f923bae3cab61a40f5ca454b70d2257b85bf
Madhusudan Rathore <rmadhusudan359@gmail.com>
2018-08-02 00:38:32 +0530

    Update practical-1-caser_cipher.py

--- ins/practical-1-caser_cipher.py
+++ ins/practical-1-caser_cipher.py
@@ -2,16 +2,16 @@ def encryption(plain_text, shift):
 	enc_str = ""
 	length = len(plain_text)
 	
-	for x in xrange(length):
-		char = plain_text[x]
-		if char == " ":
-			enc_str += char
-		elif char.isupper():
+	for index in xrange(length):
+		char = plain_text[index]
+		if char.isupper():
 			enc_str += chr((ord(char)+shift-65)%26+65)
 		elif char.islower():
 			enc_str += chr((ord(char)+shift-97)%26+97)
-		else: # for numbers and special characters
-			enc_str += chr((ord(char)+shift*10)%256)
+		elif char.isdigit():
+			enc_str += chr((ord(char)+shift-48)%10+48)
+		else: # special characters
+			enc_str += chr((ord(char)+shift-32)%16+32)
 	
 	return enc_str
 
@@ -19,22 +19,22 @@ def decryption(cipher_text, shift):
 	dec_str = ""
 	length = len(cipher_text)
 	
-	for x in xrange(length):
-		char = cipher_text[x]
-		if char == " ":
-			dec_str += char
-		elif char.isupper():
+	for index in xrange(length):
+		char = cipher_text[index]
+		if char.isupper():
 			dec_str += chr((ord(char)+(26-shift)-65)%26+65) # using cyclic property for decryption
 		elif char.islower():
 			dec_str += chr((ord(char)+(26-shift)-97)%26+97)
-		else:
-			dec_str += chr((ord(char)+(256-shift*10))%256)
+		elif char.isdigit():
+			dec_str += chr((ord(char)+(10-shift)-48)%10+48)
+		else: # special characters
+			dec_str += chr((ord(char)+(16-shift)-32)%16+32)
 	
 	return dec_str
 
 def main():
-	shift = 10
-	plain_text = "hello STUDENT 0123456789 !$%&*()-+/*.,'`~#}{<>[]}{"
+	shift = 25
+	plain_text = "hello STUDENT 0123456789 !\"#$%&'()+,-./*" # no other special characters than mentioned
 	cipher_text = encryption(plain_text, shift)
 	decipher_text = decryption(cipher_text, shift)
 
