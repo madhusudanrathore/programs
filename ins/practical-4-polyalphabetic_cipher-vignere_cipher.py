fe117c68d53c0fe8ba870e367b91b96dcd98fad9
Madhusudan Rathore <rmadhusudan359@gmail.com>
2018-08-02 01:05:06 +0530

    Update practical-4-polyalphabetic_cipher-vignere_cipher.py

--- ins/practical-4-polyalphabetic_cipher-vignere_cipher.py
+++ ins/practical-4-polyalphabetic_cipher-vignere_cipher.py
@@ -1,10 +1,11 @@
-def padding(plain_text, key): # padding for remaining values
+''' vigenere cipher '''
+def prepare_input(plain_text, key):
 	temp_str = ""
 	temp_str += plain_text
 	plain_text_len = len(plain_text)
 	key_len = len(key)
 	
-	if plain_text_len%key_len != 0:
+	if plain_text_len%key_len != 0: # padding for remaining values
 		temp = key_len-plain_text_len%key_len
 		for i in range(temp):
 			temp_str += "~"
@@ -21,14 +22,17 @@ def encryption(final_plain_text, key):
 		for y in range(key_len):
 			shift = ord(key[y])
 			char = final_plain_text[x+y]
-			if char == " ":
-				enc_str += char
+			if char.isupper():
+				enc_str += chr((ord(char)+shift-65)%26+65)
 			elif char.islower():
 				enc_str += chr((ord(char)+shift-97)%26+97)
-			elif char.isupper():
-				dec_str += chr((ord(char)+shift-65)%26+65)
+			elif char.isdigit():
+				enc_str += chr((ord(char)+shift-48)%10+48)
 			elif char == "~":
-				enc_str += '~'
+				enc_str += "~"
+			else: # special characters
+				enc_str += chr((ord(char)+shift-32)%16+32)
+		
 		x += key_len
 	
 	return enc_str
@@ -41,32 +45,35 @@ def decryption(final_plain_text, key):
 	x=0
 	while x < final_plain_text_len:
 		for y in range(key_len):
-			shift=26-ord(key[y])
+			shift=ord(key[y])
 			char=final_plain_text[x+y]
-			if char==" ":
-				dec_str += char
+			if char.isupper():
+				dec_str += chr((ord(char)+(26-shift)-65)%26+65) # using cyclic property for decryption
 			elif char.islower():
-				dec_str += chr((ord(char)+shift-97)%26+97)
-			elif char.isupper():
-				dec_str += chr((ord(char)+shift-65)%26+65)
+				dec_str += chr((ord(char)+(26-shift)-97)%26+97)
+			elif char.isdigit():
+				dec_str += chr((ord(char)+(10-shift)-48)%10+48)
 			elif char=="~":
-				dec_str += '~'
+				dec_str += ""
+			else: # special characters
+				dec_str += chr((ord(char)+(16-shift)-32)%16+32)
+
 		x += key_len
 	
 	return dec_str
 
 def main():
-	key = "M@d#U"
-	plain_text = "hellomadhusudan"
+	key = "keyword"
+	plain_text = "hello STUDENT 0123456789 !\"#$%&'()+,-./*" # no other special characters
+	final_plain_text = None
 	cipher_text = None
 	decipher_text = None
 
-	final_plain_text=padding(plain_text, key)
+	final_plain_text=prepare_input(plain_text, key)
 	cipher_text = encryption(final_plain_text, key)
 	decipher_text = decryption(cipher_text, key)
 
 	print("PLAIN TEXT:\t" + plain_text)
-	print ("FINAL PLAIN TEXT:\t" + final_plain_text)
 	print("CIPHER TEXT:\t" + cipher_text)
 	print("DECIPHER TEXT:\t" + decipher_text)
 
