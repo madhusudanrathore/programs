3d5d8406b94413dd5d278557d3d9fd3978df4b12
Madhusudan Rathore <madhusudanrathore@protonmail.com>
2018-09-17 23:48:40 +0530

    sept 17, cpp to java conversion

--- data_structures/HashTable/HashTable.java
+++ data_structures/HashTable/HashTable.java
@@ -1,228 +1,224 @@
-class Node{
-    int data=0;
-    Node prev=null, next=null;
+import java.util.Random;
 
-    Node(int value){
-    	this.data=value;
+class DoublyNode{
+    int data=0;
+    DoublyNode prev=null, next=null;
+
+    DoublyNode(int value){
+        this.data=value;
     }
 }
 
 class DoublyLinkedList{
-	Node head, temp, pre_temp, next_temp;
+    DoublyNode head, temp;
 
-	DoublyLinkedList() {
-		Node new_node=new Node(100);
-		this.head=new_node;
-	}
+    DoublyLinkedList(){
+        DoublyNode new_node=new DoublyNode(100);
+        this.head=new_node;
+    }
 
-	void insert_beg( int value ) {
-		Node new_node=new Node(value);
-		new_node.next=this.head;
-		new_node.prev=null;
-		this.head.prev=new_node;
-		this.head=this.new_node;
-	}
+    void insert_beg(int value){
+        DoublyNode new_node=new DoublyNode(value);
 
-	void insert_end(ll &val) {
+        if(this.head==null){
+            this.head=new_node;
+        }else{
+            new_node.next=this.head;
+            this.head.prev=new_node;
+            this.head=new_node;
+        }
+        System.out.println( "INSERT BEGINNING:\t" + value );
+    }
+    
+    void insert_end(int value){
+        DoublyNode new_node=new DoublyNode(value);
 
-		struct node *new_node = new struct node;
-		temp = head;
-		while (temp->next != NULL)
-			temp = temp->next;
+        if(this.head==null){
+            this.head=new_node;
+        }else{
+            this.temp=this.head;
+            
+            while(this.temp.next!=null)
+                this.temp=this.temp.next;
 
-		new_node->data = val;
-		new_node->next = NULL;
-		new_node->prev = temp;
-		temp->next = new_node;
-	}
+            new_node.prev=this.temp;
+            this.temp.next=new_node;
 
-	void delete_beg(ll &val) {
+            System.out.println( "INSERT END:\t" + value );
+        }
+    }
+    
+    void delete_beg(){
+        if(this.head==null){ // empty list
+            System.out.println( "DELETE BEGINNING:\tEMPTY LIST" );
+        }else{
+            this.temp=this.head;
+            this.head=this.head.next;
+            this.head.prev=null;
+            this.temp=null;
+            System.gc();
+        }
+    }
+    
+    void delete_end(){
+        if(this.head==null){ // empty list
+            System.out.println( "DELETE END:\tEMPTY LIST" );
+        }
+        else{
+            this.temp=this.head;
 
-		if (head->next == NULL) {//one node
-			delete head;
-			head = NULL;
-		}
-		else {
-			temp = head;
-			head = head->next;
-			head->prev = NULL;
-			delete temp;
-		}
-	}
+            while(this.temp.next!=null)
+                this.temp=this.temp.next;
 
-	void delete_end() {
-		if (head == NULL)
-			cout << "empty" << endl;
-		else {//1 node only
-			if (head->next == NULL) {
-				delete head;
-				head = NULL;
-			}
-			else {
-				temp = head;
-				while (temp->next != NULL)
-					temp = temp->next;
+            this.temp.prev.next=null;
+            this.temp=null;
+            System.gc();
+        }
+    }
 
-				temp->prev->next = NULL;
-				delete temp;
-			}
-		}
-	}
+    void delete_specific( int value ){
+        if(this.head==null){
+            System.out.println( "DELETE SPECIFIC:\tEMPTY LIST" );
+        }else if(this.head.data==value){ // first node is to be deleted
+            this.head=null;
+            System.gc();
+        }else{ // find node
+            this.temp=this.head;
 
-	void search(ll &val, ll &key) {
-		if (head == NULL)
-			cout << val << " corresponds to an INDEX " << key << " which is EMPTY" << endl;
-		else {
-			ll count = 0;
-			temp = head;
-			while (temp->data != val && temp != NULL)
-				temp = temp->next, count++;
-			if (temp != NULL)
-				cout << val << " FOUND in TABLE at INDEX " << key + 1 << " and is at POSITION number " << count + 1 << endl;
-			else cout << val << " NOT FOUND" << endl;
-		}
-	}
+            while(this.temp!=null){
+                if(this.temp.data!=value)
+                    this.temp=this.temp.next;
+                // else break;
+            }
+            if(this.temp!=null){
+                this.temp.prev.next=this.temp.next;
+                this.temp.next.prev=this.temp.prev;
+                this.temp=null;
+                System.out.println( "DELETE SPECIFIC:\t" + value + " DELETED" );
+            }else System.out.println( "DELETE SPECIFIC:\t" + value + " NOT FOUND" );
+            System.gc();
+        }
+    }
 
-	void display() {
-		temp = head;
-		if (head == NULL)
-			cout << "Empty" << endl;
-		else {
-			cout << "NULL <=> ";
-			while (temp != NULL) {
-				cout << temp->data << " <=> ";
-				temp = temp->next;
-			}
-			cout << " NULL" << endl;
-		}
-	}
-
-};
+    void display(){
+        this.temp=this.head;
+        if(this.head==null){
+            System.out.println( "DISPLAY:\tEMPTY LIST" );
+        }else{
+            System.out.print( "PRINTING LIST:\tnull <=> " );
+            
+            while(this.temp!=null){
+                System.out.print( this.temp.data + " <=> " );
+                this.temp=this.temp.next;
+            }
+            
+            System.out.println( " null" );
+        }
+    }
+}
 
 public class HashTable{
-	private size=100;
-	private DoublyLinkedList hashtable;
+	protected int size=100;
+	protected DoublyLinkedList hashtable;
+	
 	HashTable(){
-		hashtable=new DoublyLinkedList[size]; // HASH TABLE will hold HEAD REFERENCES to DOUBLY LINKED LISTS
+		// HASH TABLE wiint hold HEAD REFERENCES to DOUBLY LINKED LISTS
+		this.hashtable=new DoublyLinkedList[size];
 		
-		for (ll k = 0; k < 10000; k++)
-			hash_sample_insert(hashtable, size);
+		for (int k=0; k < 10000; k++)
+			this.hash_sample_insert();
 	}
 	
-	void hash_sample_insert(doubly_ll *hashtable, ll &size) {
-		ll val, key;
-		val = rand() % 9234;
+	void hash_sample_insert() {
+		Random rand = new Random();
+		int val, key;
+		
+		val=rand.nextInt(9235); // between 0 to 9234
 
-		key = ((val%size) + (val % (size - 1))) % size;//hash function using DOUBLE HASHING
-		//cout << "KEY = " << key << endl;
+		key=((val%size) + (val % (size - 1))) % size;//hash function using DOUBLE HASHING
 
-		if (hashtable[key].head == NULL)//empty first location of LIST
-			hashtable[key].create(val);
-
-		else//non empty first location of LIST, thus enter at beginning
-			hashtable[key].insert_beg(val);
+		// if(hashtable[key].head==null){ // empty first location of LIST
+		// 	hashtable[key].create(val);
+		// }else{ // non empty first location of LIST, thus enter at beginning
+		// 	hashtable[key].insert_beg(val);
+		// }
+		hashtable[key].insert_beg(val);
 	}
 
-	void hash_insert_beg(doubly_ll *hashtable, ll &size) {
-		ll val, key;
+	// void hash_insert_beg() {
+	// 	int val, key;
 
-		cout << "Enter a VALUE to be mapped in HASH TABLE" << endl;
-		cin >> val;
+	// 	System.out.println( "Enter a VALUE to be mapped in HASH TABLE" << endl;
+	// 	cin >> val;
 
-		key = ((val%size) + (val % (size - 1))) % size;//hash function using DOUBLE HASHING
-		cout << "KEY = " << key << endl;
+	// 	key=((val%size) + (val % (size - 1))) % size;//hash function using DOUBLE HASHING
+	// 	System.out.println( "KEY=" << key << endl;
 
-		if (hashtable[key].head == NULL)//empty first location of LIST
-			hashtable[key].create(val);
+	// 	if (hashtable[key].head==null)//empty first location of LIST
+	// 		hashtable[key].create(val);
 
-		else//non empty first location of LIST, thus enter at beginning
-			hashtable[key].insert_beg(val);
-	}
+	// 	else//non empty first location of LIST, thus enter at beginning
+	// 		hashtable[key].insert_beg(val);
+	// }
 
-	void hash_insert_end(doubly_ll *hashtable, ll &size) {
-		ll val, key;
+	// void hash_insert_end() {
+	// 	int val, key;
 
-		cout << "Enter a VALUE to be mapped in HASH TABLE" << endl;
-		cin >> val;
+	// 	System.out.println( "Enter a VALUE to be mapped in HASH TABLE" << endl;
+	// 	cin >> val;
 
-		key = ((val%size) + (val % (size - 1))) % size;//hash function using DOUBLE HASHING
-		cout << "KEY = " << key << endl;
+	// 	key=((val%size) + (val % (size - 1))) % size;//hash function using DOUBLE HASHING
+	// 	System.out.println( "KEY=" << key << endl;
 
-		if (hashtable[key].head == NULL)//empty first location of LIST
-			hashtable[key].create(val);
+	// 	if (hashtable[key].head==null)//empty first location of LIST
+	// 		hashtable[key].create(val);
 
-		else//non empty first location of LIST, thus enter at end
-			hashtable[key].insert_end(val);
-	}
+	// 	else//non empty first location of LIST, thus enter at end
+	// 		hashtable[key].insert_end(val);
+	// }
 
-	void hash_display(doubly_ll *hashtable, ll &size) {
-		for (ll i = 0; i < size; i++) {
-			cout << "HT[" << i+1 << "]\t";
+	// void hash_delete_beg() {
+	// 	int val, key;
+
+	// 	System.out.println( "Enter a VALUE to be DELETED from HASH TABLE" << endl;
+	// 	cin >> val;
+
+	// 	key=val % 9;
+
+
+	// 	if (hashtable[key].head==null) {//empty location
+	// 		System.out.println( "Empty Hash" << endl;
+	// 	}
+	// 	else {
+	// 		hashtable[key].delete_beg(key);
+	// 	}
+	// }
+
+	// void hash_search() {
+	// 	int val, key;
+
+	// 	System.out.println( "Enter a VALUE to be searched in HASH TABLE" << endl;
+	// 	cin >> val;
+
+	// 	key=((val%size) + (val % (size - 1))) % size;//hash function using DOUBLE HASHING
+	// 	hashtable[key].search(val, key);
+	// }
+
+	void hash_display() {
+		for (int i=0; i < size; i++) {
+			System.out.println( "HT[" + (i+1) + "]\t" );
 			hashtable[i].display();
 		}
 	}
 
-	void hash_delete_beg(doubly_ll *hashtable) {
-		ll val, key;
-
-		cout << "Enter a VALUE to be DELETED from HASH TABLE" << endl;
-		cin >> val;
-
-		key = val % 9;
-
-
-		if (hashtable[key].head == NULL) {//empty location
-			cout << "Empty Hash" << endl;
-		}
-		else {
-			hashtable[key].delete_beg(key);
-		}
-	}
-
-	void hash_search( doubly_ll *hashtable, ll &size ) {
-		ll val, key;
-
-		cout << "Enter a VALUE to be searched in HASH TABLE" << endl;
-		cin >> val;
-
-		key = ((val%size) + (val % (size - 1))) % size;//hash function using DOUBLE HASHING
-		hashtable[key].search(val, key);
-	}
 	public static void main(String[] args){
-		
 
-		while(true){
-			cout << "Enter Your Choice.." << endl;
-			cout << "1). INSERT Entry at BEGINNING" << endl;
-			cout << "2). INSERT Entry at END" << endl;
-			cout << "3). Delete an Entry" << endl;
-			cout << "4). Display Current Hash" << endl;
-			cout << "5). SEARCH for a Value" << endl;
-			cout << "6). EXIT" << endl;
-			cin >> ch;
+		HashTable table=new HashTable();
 
-			switch (ch) {
-	            case 1:
-	                hash_insert_beg(hashtable, size);
-	                break;
-	            case 2:
-	                hash_insert_end(hashtable, size);
-	                break;
-	            case 3:
-	                hash_delete_beg(hashtable);
-	                break;
-	            case 4:
-	                hash_display(hashtable, size);
-	                break;
-	            case 5:
-	                hash_search(hashtable, size);
-	                break;
-	            case 6:
-	                return 0;
-	            default:
-	                cout << "INVALID OPTION" << endl;
-	                break;
-			}
-	    }
+		// hash_insert_beg();
+		// hash_insert_end();
+		// hash_delete_beg();
+		hash_display();
+		// hash_search();
 	}
 }
--- data_structures/LinkedList/DoublyLinkedList.java
+++ data_structures/LinkedList/DoublyLinkedList.java
@@ -14,10 +14,11 @@ class DoublyLinkedList{
         DoublyNode new_node=new DoublyNode(100);
         this.head=new_node;
     }
+
     void insert_beg(int value){
         DoublyNode new_node=new DoublyNode(value);
 
-        if (this.head==null){
+        if(this.head==null){
             this.head=new_node;
         }else{
             new_node.next=this.head;
@@ -26,6 +27,7 @@ class DoublyLinkedList{
         }
         System.out.println( "INSERT BEGINNING:\t" + value );
     }
+    
     void insert_end(int value){
         DoublyNode new_node=new DoublyNode(value);
 
@@ -35,47 +37,67 @@ class DoublyLinkedList{
             this.temp=this.head;
             
             while(this.temp.next!=null)
-                this.temp = this.temp.next;
+                this.temp=this.temp.next;
 
-            new_node.next=null;
             new_node.prev=this.temp;
             this.temp.next=new_node;
 
             System.out.println( "INSERT END:\t" + value );
         }
     }
-    void insert_before(){
-        if (this.head == null)//empty
-            System.out.println << "Empty, Create first" << endl;
-        else {//not empty
-            struct node *new_node = new struct node;
-            int val;
-            this.temp = this.head;
-            System.out.println << "Enter Value Before which DATA is to be added\t";
-            cin >> val;
-            if (this.temp.data == val)//to be added before first element
-                this.insert_beg();
-            else {//
-                while (this.temp.data != val && this.temp.next != null) {
-                    this.temp = this.temp.next;
-                }
-                if (this.temp.data == val) {//node found
-                    System.out.println << "Enter Data to be Added\t";
-                    cin >> new_node.data;
-                    new_node.next = this.temp;
-                    new_node.prev = this.temp.prev;
-                    this.temp.prev.next = new_node;
-                    this.temp.prev = new_node;
-                }
-                else if(this.temp.data != val){//node not found
-                    System.out.println << val << " Not Found" << endl;
-                }
+    
+    void insert_before(int before_value, int value){
+        if(this.head==null){ // empty
+            System.out.println( "INSERT BEFORE:\tEMPTY LIST" );
+        }else if(this.head.data==before_value){ // to be added before first element
+            this.insert_beg(value);
+        }else{ // search and insert
+            this.temp=this.head;
+
+            while(this.temp.data!=before_value && this.temp.next!=null){ // untill node found or null
+                this.temp=this.temp.next;
+            }
+            if(this.temp.data==before_value){ // node found
+                DoublyNode new_node=new DoublyNode(value);
+                new_node.next=this.temp;
+                new_node.prev=this.temp.prev;
+                this.temp.prev.next=new_node;
+                this.temp.prev=new_node;
+                System.out.println( "INSERT BEFORE:\t" + value + " INSERTED" );
+            }else if(this.temp.data!=before_value){ // node not found
+                System.out.println( "INSERT BEFORE:\t" + before_value + " NOT FOUND" );
             }
         }
     }
+    
+    // void insert_after(int after_value, int value){
+    //     if(this.head==null){ // empty list
+    //         System.out.println( "INSERT AFTER:\tEMPTY LIST" );
+    //     }else if(this.head.next==null && this.temp.data==after_value){ // after first element
+    //             this.insert_end(value);
+    //     }else{ // search and insert
+    //         this.temp=this.head;
+
+    //         while(this.temp.data!=after_value && this.temp.next!=null){ // untill node found or null
+    //             this.temp=this.temp.next;
+    //         }
+
+    //         if(this.temp.data==after_value){ // node found
+    //             DoublyNode new_node=new DoublyNode(value);
+    //             new_node.next=this.temp.next;
+    //             new_node.prev=this.temp;
+    //             this.temp.next.prev=new_node;
+    //             this.temp.next=new_node;
+    //             System.out.println( "INSERT AFTER:\t" + value + " INSERTED" );
+    //         }else if(this.temp.data!=after_value){ // node not found
+    //             System.out.print( "INSERT AFTER:\t" + after_value + " NOT FOUND" );
+    //         }
+    //     }
+    // }
+    
     void delete_beg(){
         if(this.head==null){ // empty list
-            System.out.println( "EMPTY LIST" );
+            System.out.println( "DELETE BEGINNING:\tEMPTY LIST" );
         }else{
             this.temp=this.head;
             this.head=this.head.next;
@@ -84,9 +106,10 @@ class DoublyLinkedList{
             System.gc();
         }
     }
+    
     void delete_end(){
         if(this.head==null){ // empty list
-            System.out.println( "EMPTY LIST" );
+            System.out.println( "DELETE END:\tEMPTY LIST" );
         }
         else{
             this.temp=this.head;
@@ -99,14 +122,92 @@ class DoublyLinkedList{
             System.gc();
         }
     }
-    void display() {
+
+    void delete_specific( int value ){
+        if(this.head==null){
+            System.out.println( "DELETE SPECIFIC:\tEMPTY LIST" );
+        }else if(this.head.data==value){ // first node is to be deleted
+            this.head=null;
+            System.gc();
+        }else{ // find node
+            this.temp=this.head;
+
+            while(this.temp!=null){
+                if(this.temp.data!=value)
+                    this.temp=this.temp.next;
+                // else break;
+            }
+            if(this.temp!=null){
+                this.temp.prev.next=this.temp.next;
+                this.temp.next.prev=this.temp.prev;
+                this.temp=null;
+                System.out.println( "DELETE SPECIFIC:\t" + value + " DELETED" );
+            }else System.out.println( "DELETE SPECIFIC:\t" + value + " NOT FOUND" );
+            System.gc();
+        }
+    }
+
+    // void delete_before(){
+    //     if(this.head==null){ // empty list
+    //         System.out.print( "Empty, Create first" );
+    //     }else{ // list not empty
+    //         this.temp=this.head;
+
+    //         if(this.temp.data==value)//first node
+    //             System.out.print( "NO NODE before First Element" );
+    //         else if(this.temp.next.data==value)//delete first node
+    //             this.delete_beg();
+    //         else{//find node
+    //             while(this.temp.data!=value && this.temp.next!=null){
+    //                 this.temp=this.temp.next;
+    //             }
+    //             if(this.temp.data==value){//found
+    //                 pre_this.temp=this.temp.prev;
+    //                 pre_this.temp.prev.next=this.temp;
+    //                 this.temp.prev=pre_this.temp.prev;
+
+    //                 delete pre_this.temp;
+    //             }
+    //             else{
+    //                 System.out.print( value + " Not Found" );
+    //             }
+
+    //         }
+    //     }
+    // }
+
+    // void delete_after(){
+    //     if(this.head==null)
+    //         System.out.print( "Empty, Create first" );
+    //     else{
+    //         int value;
+    //         this.temp=this.head;
+    //         System.out.print( "Enter NODE value AFTER which DATA is to be DELETED\t" );
+    //         cin >> value;
+    //         while(this.temp.next!=null && this.temp.data!=value){
+    //             this.temp=this.temp.next;
+    //         }
+    //         if(this.temp.next==null && this.temp.data==value)//last node
+    //             System.out.print( "Last Element, Cannot Delete" );
+    //         else if(this.temp.data!=value){
+    //             System.out.print( value + " Not Found" );
+    //         }
+    //         else{
+    //             next_this.temp=this.temp.next;
+    //             this.temp.next=next_this.temp.next;
+    //             next_this.temp.next.prev=this.temp;
+    //         }
+    //     }
+    // }
+
+    void display(){
         this.temp=this.head;
-        if (this.head==null){
-            System.out.println( "EMPTY" );
+        if(this.head==null){
+            System.out.println( "DISPLAY:\tEMPTY LIST" );
         }else{
             System.out.print( "PRINTING LIST:\tNULL <=> " );
             
-            while (this.temp!=null) {
+            while(this.temp!=null){
                 System.out.print( this.temp.data + " <=> " );
                 this.temp=this.temp.next;
             }
@@ -114,6 +215,7 @@ class DoublyLinkedList{
             System.out.println( " NULL" );
         }
     }
+    
     public static void main(String[] args){
         DoublyLinkedList linked_list=new DoublyLinkedList();
 
@@ -121,8 +223,8 @@ class DoublyLinkedList{
             linked_list.insert_beg(i+1);
             linked_list.insert_end(i+6);
 
-            // linked_list.insert_before(i+1);
-            // linked_list.insert_after(i+6);
+            // linked_list.insert_before(i+1, (i+1)*1000);
+            // linked_list.insert_after(i+6, (i+6)*1000);
         }
         linked_list.display();
 
@@ -132,129 +234,13 @@ class DoublyLinkedList{
         // }
         // linked_list.display();
 
-        // for(int i=0; i<5; ++i){
-        //     linked_list.delete_beg();
-        //     linked_list.delete_end();
-        // }
-        // linked_list.display();
+        for(int i=0; i<5; ++i){
+            linked_list.delete_beg();
+            linked_list.delete_end();
+        }
+        linked_list.display();
 
-        // linked_list.delete_specific(100);
-        // linked_list.display();
+        linked_list.delete_specific(100);
+        linked_list.display();
     }
 }
-
-        // void insert_after(){
-        //     if (this.head == null)//empty list
-        //         System.out.println << "Empty, Create first" << endl;
-        //     else {//more than 1 element
-        //         struct node *new_node = new struct node;
-        //         int val;
-        //         this.temp = this.head;
-        //         System.out.println << "Enter Value After which DATA is to be added\t";
-        //         cin >> val;
-
-        //         while (this.temp.data != val && this.temp.next != null) {
-        //             this.temp = this.temp.next;
-        //         }
-        //         if (this.temp.next == null && this.temp.data == val )
-        //             this.insert_end();
-
-        //         else if (this.temp.data == val) {
-        //             System.out.println << "Enter Data to be Added\t";
-        //             cin >> new_node.data;
-
-        //             new_node.next = this.temp.next;
-        //             new_node.prev = this.temp;
-        //             this.temp.next.prev = new_node;
-        //             this.temp.next = new_node;
-        //         }
-        //         else{
-        //             System.out.println << val << " Not Found" << endl;
-        //         }
-        //     }
-        // }
-
-        // void delete_before(){
-        //     if (this.head == null)//empty list
-        //         System.out.println << "Empty, Create first" << endl;
-        //     else {//list not empty
-        //         int val;
-        //         this.temp = this.head;
-        //         System.out.println << "Enter Value Before which DATA is to be DELETED\t";
-        //         cin >> val;
-
-        //         if (this.temp.data == val)//first node
-        //             System.out.println << "NO NODE before First Element" << endl;
-        //         else if (this.temp.next.data == val)//delete first node
-        //             this.delete_beg();
-        //         else {//find node
-        //             while (this.temp.data != val && this.temp.next != null) {
-        //                 this.temp = this.temp.next;
-        //             }
-        //             if (this.temp.data == val) {//found
-        //                 pre_this.temp = this.temp.prev;
-        //                 pre_this.temp.prev.next = this.temp;
-        //                 this.temp.prev = pre_this.temp.prev;
-
-        //                 delete pre_this.temp;
-        //             }
-        //             else{
-        //                 System.out.println << val << " Not Found" << endl;
-        //             }
-
-        //         }
-        //     }
-        // }
-
-        // void delete_after() {
-        //     if (this.head == null)
-        //         System.out.println << "Empty, Create first" << endl;
-        //     else {
-        //         int val;
-        //         this.temp = this.head;
-        //         System.out.println << "Enter NODE Value AFTER which DATA is to be DELETED\t";
-        //         cin >> val;
-        //         while (this.temp.next != null && this.temp.data != val) {
-        //             this.temp = this.temp.next;
-        //         }
-        //         if (this.temp.next == null && this.temp.data == val)//last node
-        //             System.out.println << "Last Element, Cannot Delete" << endl;
-        //         else if (this.temp.data != val) {
-        //             System.out.println << val << " Not Found" << endl;
-        //         }
-        //         else{
-        //             next_this.temp = this.temp.next;
-        //             this.temp.next = next_this.temp.next;
-        //             next_this.temp.next.prev = this.temp;
-        //         }
-        //     }
-        // }
-
-        // void delete_specific() {
-        //     if (this.head == null)
-        //         System.out.println << "empty" << endl;
-        //     else {
-        //         int val;
-        //         System.out.println << "Enter Value to be Deleted\t";
-        //         cin >> val;
-        //         this.temp = this.head;
-
-        //         if (this.temp.data == val)//first node is to be deleted then use DELETE at BEGINNING
-        //             delete_beg();
-
-        //         else {// find node
-        //             while (this.temp != null) {
-        //                 if(this.temp.data!=val)
-        //                     this.temp = this.temp.next;
-        //                 else break;
-        //             }
-        //             if (this.temp != null) {
-        //                 this.temp.prev.next = this.temp.next;
-        //                 this.temp.next.prev = this.temp.prev;
-        //                 delete this.temp;
-        //                 System.out.println << val << " Deleted" << endl;
-        //             }
-        //             else System.out.println << val << " Not Found" << endl;
-        //         }
-        //     }
-        // }
